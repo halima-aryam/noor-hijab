02f7213fe7c9a07abeab6bdef8e9711175b7f9bb
halima-aryam <salaammaryamm@gmail.com>
2026-04-07 09:59:45 +0300

    fix to sveltia cms

--- api/auth.js
+++ api/auth.js
@@ -1,32 +1,36 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
 module.exports = async (req, res) => {
   const { code } = req.query;
-  
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code
-    })
-  });
 
-  const data = await response.json();
-  
-  res.send(`
-    <script>
-      window.opener.postMessage(
-        'authorization:github:success:${JSON.stringify(data)}',
-        '*'
-      );
-    </script>
-  `);
+  const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+  const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+  try {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code
+      })
+    });
+
+    const data = await response.json();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`
+      <script>
+        window.opener.postMessage(
+          'authorization:github:success:${JSON.stringify(data)}',
+          '*'
+        );
+        window.close();
+      </script>
+    `);
+  } catch(err) {
+    res.status(500).send('Auth error: ' + err.message);
+  }
 };
